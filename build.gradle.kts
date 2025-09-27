30c6668f3a964c22514344da22252fa3bfa13bea
astor-dev <orangnlp@gmail.com>
2025-09-27 13:26:05 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("java")
+}
+
+group = "bottom-up-java-web"
+version = "1.0-SNAPSHOT"
+
+subprojects {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "bottom-up-java-web"
+include("1-java-socket")
+include("2-java-http")
